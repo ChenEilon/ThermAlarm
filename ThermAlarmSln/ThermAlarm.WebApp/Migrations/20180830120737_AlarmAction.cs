aa4caab6925405f2cd5d8edba2ad20ce8984b85d
unknown <cheneilon@gmail.com>
2018-08-30 15:16:52 +0300

    add migration

---
+++ ThermAlarmSln/ThermAlarm.WebApp/Migrations/20180830120737_AlarmAction.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThermAlarm.WebApp.Migrations
+{
+    public partial class AlarmAction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "lastName",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstName",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BTid",
+                table: "Person",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateTime",
+                table: "Msg",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                defaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.CreateTable(
+                name: "AlarmAction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateTime = table.Column<DateTime>(nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlarmAction", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlarmAction");
+
+            migrationBuilder.DropColumn(
+                name: "CreateTime",
+                table: "Msg");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastName",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstName",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BTid",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}

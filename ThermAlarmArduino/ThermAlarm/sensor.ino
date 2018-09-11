08e8e771119ad136bc32e0cd52d55c257a3e4e48
unknown <cheneilon@gmail.com>
2018-09-11 17:21:29 +0300

    clean debug prints

--- ThermAlarmArduino/ThermAlarm/sensor.ino
+++ ThermAlarmArduino/ThermAlarm/sensor.ino
@@ -1,12 +1,5 @@
 uint8_t getPirData(void) { //EXAMPLE INCLUDES MORE COMPLEX PIR VALUE DEFINITION
    uint8_t pirStatus = digitalRead(PIR_PIN);
-   //uint8_t pirStatustus = HIGH; //TODO - switch with upper row
-   if(pirStatus == HIGH) {
-      Serial.println("PIR - Motion detected.");//DEBUG
-   }
-   if(pirStatus == LOW) {
-      Serial.println("PIR - Motion ended."); //DEBUG
-   }
    return pirStatus;
 }
 
